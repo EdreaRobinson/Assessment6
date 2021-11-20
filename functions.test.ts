dad32032916caf1494ec7102a7a61339749f8927
Edrea Robinson <edrea1979@gmail.com>
2021-11-20 14:12:31 -0600

    Part 3: Unit Tests completed

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,23 @@
+import { convertToObject } from "typescript"
+
 const {shuffleArray} = require('./utils')
+
+const array = [1, 2, 3, 4, 5]
+const shuffledArray = shuffleArray(array)
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    test('shuffleArray should return an array with length 2', () => {
+        expect(shuffledArray).toHaveLength(2)
+})    
+
+    test('shuffleArray should contain the same items', () => {
+        expect(shuffledArray.sort).toEqual(array.sort);
 })
+
+    test('shuffleArray should be defined', () => {
+        expect(shuffledArray).toBeDefined()
+})
+
+})
+
