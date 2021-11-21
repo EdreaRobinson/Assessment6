b8f48d8872f98509cd20604405100e2a70f99cc4
Edrea Robinson <edrea1979@gmail.com>
2021-11-20 19:47:20 -0600

    Part 4: Write Automated Tests completed

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -5,6 +5,7 @@ require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
+
 beforeEach(async () => {
     driver.get('http://localhost:3000/')
 })
@@ -17,4 +18,23 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+})
+
+test('Add to Duo button displays player-duo', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(2000)
+    await driver.findElement(By.className('bot-btn')).click()
+    await driver.sleep(2000)
+    const playerId = await driver.findElement(By.id('player-duo'))
+    const displayed= await playerId.isDisplayed()
+    expect(displayed).toBe(true)
+    await driver.sleep(2000)
+})
+
+test('Draw button diplays choices', async () => {
+    await driver.findElement(By.id('draw')).click()
+    const choices = await driver.findElement(By.id('choices'))
+    const displayed = await choices.isDisplayed()
+    expect(displayed).toBe(true)
+    await driver.sleep(2000)
 })
